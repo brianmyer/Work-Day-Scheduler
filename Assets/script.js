b17b59c0864247d1680fffa2c834364d3040b893
brianmyer <myer.brian@gmail.com>
2023-08-02 09:49:14 -0400

    localstorage now working with all unique functions

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,6 @@
 // in the html.
 let hours = $('.row');
 let activity = $('.description');
-let activities = JSON.parse(localStorage.getItem('activities')) || [];
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,10 +13,63 @@ $(function () {
   $('#hour-9').on('click', function(event) {
     if (event.target.matches(".btn") || event.target.matches(".fas")) {
     console.log('click9');
-    event.preventDefault();
-    let hour9 = {'9': activity[0].value};
-    activities.push(hour9);
-    localStorage.setItem('activities', JSON.stringify(activities))
+    localStorage.setItem('activity9', activity[0].value)
+    }
+  })
+
+  $('#hour-10').on('click', function(event) {
+    if (event.target.matches(".btn") || event.target.matches(".fas")) {
+    console.log('click10');
+    localStorage.setItem('activity10', activity[1].value)
+    }
+  })
+
+  $('#hour-11').on('click', function(event) {
+    if (event.target.matches(".btn") || event.target.matches(".fas")) {
+    console.log('click11');
+    localStorage.setItem('activity11', activity[2].value)
+    }
+  })
+
+  $('#hour-12').on('click', function(event) {
+    if (event.target.matches(".btn") || event.target.matches(".fas")) {
+    console.log('click12');
+    localStorage.setItem('activity12', activity[3].value)
+    }
+  })
+
+  $('#hour-13').on('click', function(event) {
+    if (event.target.matches(".btn") || event.target.matches(".fas")) {
+    console.log('click13');
+    localStorage.setItem('activity13', activity[4].value)
+    }
+  })
+
+  $('#hour-14').on('click', function(event) {
+    if (event.target.matches(".btn") || event.target.matches(".fas")) {
+    console.log('click14');
+    localStorage.setItem('activity14', activity[5].value)
+    }
+  })
+
+  $('#hour-15').on('click', function(event) {
+    if (event.target.matches(".btn") || event.target.matches(".fas")) {
+    console.log('click15');
+    localStorage.setItem('activity15', activity[6].value)
+    }
+  })
+
+  $('#hour-16').on('click', function(event) {
+    if (event.target.matches(".btn") || event.target.matches(".fas")) {
+    console.log('click16');
+    localStorage.setItem('activity16', activity[7].value)
+    }
+  })
+
+  $('#hour-17').on('click', function(event) {
+    if (event.target.matches(".btn") || event.target.matches(".fas")) {
+    console.log('click17');
+    localStorage.setItem('activity17', activity[8].value)
     }
   })
   // TODO: Add code to apply the past, present, or future class to each time
@@ -45,7 +97,16 @@ for (let i = 0; i < hours.length; i++) {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  activity[0].value = JSON.parse(localStorage.getItem('activities'));
+  activity[0].value = localStorage.getItem('activity9');
+  activity[1].value = localStorage.getItem('activity10');
+  activity[2].value = localStorage.getItem('activity11');
+  activity[3].value = localStorage.getItem('activity12');
+  activity[4].value = localStorage.getItem('activity13');
+  activity[5].value = localStorage.getItem('activity14');
+  activity[6].value = localStorage.getItem('activity15');
+  activity[7].value = localStorage.getItem('activity16');
+  activity[8].value = localStorage.getItem('activity17');
+
   //
   // TODO: Add code to display the current date in the header of the page.
   $('#currentDay').text(dayjs().format('dddd, MMM DD, YYYY'));
